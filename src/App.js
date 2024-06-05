2bf13576a4e3480891d5ca606036bd3d4bd31e14
DharmaPalli <dharma.palli@gmail.com>
2024-06-05 22:46:31 +0530

    changes are regarding the homepage contianer added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World !!!
-        </p>
-        
-        <p>Hello Dharma </p>
-      
-      </header>
+    <div className="container">
+      <div className="row">
+        <nav>
+          <ul>
+            <li> List1</li>
+            <li> List2 </li>
+            <li> List3</li>
+            <li> List4 </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
