3e037a7d163f7f04564547df625d813f9f0f1f7e
DharmaPalli <dharma.palli@gmail.com>
2024-06-05 22:38:09 +0530

    intial commit to my branch

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World !!!
+        </p>
+        
+        <p>Hello Dharma </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
